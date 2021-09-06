f7ed6912d02a2f731d1ed53b80ab4cf16454d356
parkinglotmanagement <90092729+parkinglotmanagement@users.noreply.github.com>
2021-09-06 09:30:15 +0800

    Add files via upload

--- register.php
+++ register.php
@@ -2,144 +2,68 @@
 <html>
 <meta name="viewport" content="width=device-width; initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-
+<link rel = "stylesheet" type = "text/css" href = "./css/styles.css">  
 <style>
     body {
-        background-image: url('./background.jpg');
+        background-image: url('./images/background.jpg');
         background-position: center center;
         background-repeat: no-repeat;
         background-size: cover;
-    }
-
-    .container {
-        margin-right: 25%;
-        margin-left: 25%;
-        margin-top: 10%;
-        margin-bottom: 10%;
-        width: 50%;
-        height: 10%;
-        padding: 10%;
-        opacity: 0.89;
-        border-radius: 10px;
-        background-color: #405840;
-    }
-
-    .title {
-        width: 100%;
-        height: 69px;
-        margin: 10%;
-        opacity: 0.89;
         font-family: OpenSans;
-        font-size: 36px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: center;
-        color: #fff;
-    }
-
-    .input {
-        width: 100%;
-        height: 100%;
-        padding: 5%;
-        margin: 5%;
-        opacity: 0.89;
-        border-radius: 10px;
-        background-color: #fff;
-        font-family: OpenSans;
-        font-size: 14px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: left;
-        color: rgba(0, 0, 0, 0.25);
-    }
-
-    .button {
-        width: 50%;
-        height: 100%;
-        margin-left: 30%;
-        padding: 3%;
-        opacity: 0.89;
-        border-radius: 20px;
-        background-color: #0ba9ff;
-        font-family: OpenSans;
-        font-size: 16px;
-        font-weight: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: center;
-        color: #fff;
     }
 </style>
 
 <body>
+<?php
+    require('./db.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['email'])) {
+        // removes backslashes
+        $email = stripslashes($_REQUEST['email']);
+        //escapes special characters in a string
+        $email = mysqli_real_escape_string($con, $email);
+        $fullname    = stripslashes($_REQUEST['fullname']);
+        $fullname    = mysqli_real_escape_string($con, $fullname);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $create_datetime = date("Y-m-d H:i:s");
+        $query    = "INSERT into `users` (fullname, password, email, create_datetime)
+        VALUES ('$fullname', '" . md5($password) . "', '$email', '$create_datetime')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='index.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='register.php'>Registration</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
     <div class=" container">
         <span class="title">PLMS</span>
-        br>
-        <form name="form1" method="post" action="">
-            <input class="input" placeholder="Full Name" name="fname">
+        <br>
+        <form method="post" action="">
+            <input class="input" type="text" placeholder="Full Name" name="fullname">
             <br>
-            <input class="input" placeholder="Email" name="email">
             <br>
-            <input class="input" placeholder="Password" name="pwd">
+            <input class="input" type="text" placeholder="Email" name="email">
             <br>
-            <button button class="button">REGISTER</button>
+            <br>
+            <input class="input" type="password" placeholder="Password" name="password">
+            <br>
+            <br>
+            <button button class="button" name="submit" href="index.php">REGISTER</button>
         </form>
 
     </div>
+    <?php
+    }
+?>
 
-
-    <script type="module">
-        // Import the functions you need from the SDKs you need
-        import {
-            initializeApp
-        } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-        import {
-            getAnalytics
-        } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyAeaehNlJAJRYMHMOcgxgckq6mDk5Oe33o",
-            authDomain: "parkinglotmanagement-1c791.firebaseapp.com",
-            projectId: "parkinglotmanagement-1c791",
-            storageBucket: "parkinglotmanagement-1c791.appspot.com",
-            messagingSenderId: "396616038600",
-            appId: "1:396616038600:web:814a8aa3b946bea8928dfd",
-            measurementId: "G-HRLQHLHSL5"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-
-        var messagesRef = firebase.database().ref('registermessage');
-
-        $('#contactForm').submit(function(e) {
-            e.preventDefault();
-
-            var newMessageRef = messagesRef.push();
-            newMessageRef.set({
-                name: $('.fullname').val(),
-                email: $('.email').val(),
-                subject: $('.subject').val(),
-                message: $('.message').val()
-            });
-
-            $('.success-message').show();
-
-            $('#contactForm')[0].reset();
-        });
-    </script>
 </body>
 
 </html>
--- user_account.php
+++ user_account.php
@@ -4,113 +4,17 @@
 <head>
     <script type="text/javascript" src="./js/table_script.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel = "stylesheet" type = "text/css" href = "./css/styles.css">    
     <style>
         body {
-            background-image: url('./background.jpg');
+            background-image: url('./images/background.jpg');
             background-position: center center;
             background-repeat: no-repeat;
             background-size: cover;
             font-family: OpenSans;
         }
 
-        .container {
-            margin-right: 20%;
-            margin-left: 20%;
-            margin-top: 10%;
-            margin-bottom: 10%;
-            width: 60%;
-            height: 10%;
-            padding: 10%;
-            opacity: 0.89;
-            border-radius: 10px;
-            background-color: #405840;
-        }
-
-        .sidenav {
-            height: 100%;
-            width: 0;
-            position: fixed;
-            z-index: 1;
-            top: 0;
-            left: 0;
-            background-color: #405840;
-            overflow-x: hidden;
-            transition: 0.5s;
-            padding-top: 60px;
-        }
-
-        .sidenav a {
-            padding: 8px 8px 8px 32px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #ffff;
-            display: block;
-            transition: 0.3s;
-        }
-
-        .sidenav a:hover {
-            color: #f1f1f1;
-        }
-
-        .sidenav .closebtn {
-            position: absolute;
-            top: 0;
-            right: 25px;
-            font-size: 36px;
-            margin-left: 50px;
-        }
-
-        @media screen and (max-height: 450px) {
-            .sidenav {
-                padding-top: 15px;
-            }
-
-            .sidenav a {
-                font-size: 18px;
-            }
-        }
-
-        i.custom {
-            font-size: 6em;
-            color: green;
-        }
-
-        .logoutLblPos {
-            position: fixed;
-            right: 10px;
-            top: 5px;
-            font-size: 20px;
-        }
-
-        #parking {
-            font-family: Arial, Helvetica, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        #parking td,
-        #parking th {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
-
-        #parking tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        #parking tr:hover {
-            background-color: #ddd;
-        }
-
-        #parking th {
-            padding-top: 12px;
-            padding-bottom: 12px;
-            text-align: left;
-            background-color: #04AA6D;
-            color: white;
-        }
     </style>
 </head>
 
@@ -121,7 +25,7 @@
         <a href="./dashboard.php"><i class="material-icons">directions_car</i> SLOTS</a>
         <a href="./user_account.php"><i class="material-icons">account_circle</i> ACCOUNT</a>
     </div>
-    <div class=" container">
+    <div class=" container_dashboard">
         <table style="width:100%" id="parking" cellpadding="5">
             <tr>
                 <th>NAME</th>
